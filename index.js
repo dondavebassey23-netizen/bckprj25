62b609367f8f7183b18e562361316186ebc54bb4
David Bassey <dondave.bassey23@gmail.com>
2025-11-12 12:58:39 +0000

    made changes to index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5000",
+    origin: ["http://localhost:5000", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: false,
     allowedHeaders: ["Content-Type", "Authorization"],
